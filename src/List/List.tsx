19fd691c2cbf5111fd3ab60092ab06d33bd55aca
Philipp Kollmar <ph-kollmar@web.de>
2021-03-26 12:43:49 +0100

    #12 added div to list button

--- src/List/List.tsx
+++ src/List/List.tsx
@@ -1,8 +1,13 @@
+import App from "../App"
+
 export default function List() {
     return <div>
+        <Header />
         <h2>Einkaufszettel</h2>
+        <div>
         <input className="ListInput"></input>
         <button className="ListButton"></button>
+        </div>
         <SingleList />
         <SingleList />
         <SingleList />
@@ -13,4 +18,8 @@ export default function List() {
 
 function SingleList() {
     return <div><p>02.01.2021</p> <button className="DeleteButton"></button> </div>
+}
+
+function Header () {
+ return <div className="Header">Einkaufszettel{" > "}list</div>
 }
