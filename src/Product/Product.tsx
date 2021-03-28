658da10ff47b63994852cdab515b8f67ce61206f
Philipp Kollmar <ph-kollmar@web.de>
2021-03-28 14:42:57 +0200

    #13 added Product component

---
+++ src/Product/Product.tsx
@@ -0,0 +1,9 @@
+import '../List/List.css';
+
+export default function Product() {
+    return <div className="Product">
+        <p>02.01.2021</p>
+        <button className="UpButton">up</button>
+        <button className="UpButton">down</button>
+    </div>
+}
