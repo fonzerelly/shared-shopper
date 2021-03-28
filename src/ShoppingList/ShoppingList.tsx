c0d1d8580ff6e2de46a289170cfa3ed16a9ce0e4
Philipp Kollmar <ph-kollmar@web.de>
2021-03-28 14:51:07 +0200

    #13 added Routing to ShoppingList

--- src/ShoppingList/ShoppingList.tsx
+++ src/ShoppingList/ShoppingList.tsx
@@ -1,7 +1,15 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 import App from "../App"
 import '../List/List.css';
 import Header from '../Header/Header'
 import Product from '../Product/Product'
+
 
 
 export default function ShoppingList() {
@@ -9,25 +17,24 @@ export default function ShoppingList() {
         <Header />
         <h1>29.03.2021</h1>
         <div className="tab">
-            <button className="tablinks" >London</button>
-            <button className="tablinks" >Paris</button>
+            <Link to="/list/shoppinglist/bearbeiten"><button className="tablinks" >Bearbeiten</button></Link>
+            <Link to="/list/shoppinglist/kaufen"><button className="tablinks" >Kaufen</button></Link>
         </div>
-
-        <div id="London" className="tabcontent">
-            <h3>London</h3>
-            <p>London is the capital city of England.</p>
-        </div>
-
-        <div id="Paris" className="tabcontent">
-            <h3>Paris</h3>
-            <p>Paris is the capital of France.</p>
-        </div>
-
-        <div className="Add">
-            <input className="ListInput"></input>
-            <button className="ListButton">+</button>
-        </div>
-
-        <Product />
+        <Switch>
+            <Route path = "/list/shoppinglist/bearbeiten">
+                <div className="Add">
+                    <input className="ListInput"></input>
+                    <button className="ListButton">+</button>
+                </div>
+                <Product />
+            </Route>
+            <Route path = "/list/shoppinglist/kaufen">
+                <div className="Add">
+                    <input className="ListInput"></input>
+                    <button className="ListButton">+</button>
+                </div>
+                <Product />
+            </Route>
+        </Switch>
     </div>;
 }
