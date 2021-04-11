4bb27df513d63a52f24b9328515347f9613fd1a4
Stefan Golker <stefangolker@Stefans-MBP.fritz.box>
2021-04-11 13:33:20 +0200

    #14 added tabs to buy and change site

--- src/ShoppingList/ShoppingList.tsx
+++ src/ShoppingList/ShoppingList.tsx
@@ -1,4 +1,4 @@
-import React, {  MouseEvent } from 'react';
+import React, {  MouseEvent, useState } from 'react';
 import {
     Switch,
     Route,
@@ -14,16 +14,20 @@ import {ProductBuy, ProductBuyChecked} from '../Components/ProductBuy'
 
 
 export default function ShoppingList() {
-
+    const [toggleState, setToggleState] = useState(1);
+    const toggleTab = (index: number) => {
+        setToggleState(index)
+    }
     return <div>
         <Header />
         <h1>Einkaufsliste</h1>
         <div className="tab">
-            <Link to="/list/shoppinglist/bearbeiten"><button className="tabLinks">Bearbeiten</button></Link>
-            <Link to="/list/shoppinglist/kaufen"><button className="tabLinks">Kaufen</button></Link>
+            <Link to="/list/shoppinglist/bearbeiten"><button className={ toggleState === 1 ? "tabs active-tabs" : "tabs" } onClick={() => toggleTab(1)}>Bearbeiten</button></Link>
+            <Link to="/list/shoppinglist/kaufen"><button className={ toggleState === 2 ? "tabs active-tabs" : "tabs" } onClick={() => toggleTab(2)}>Kaufen</button></Link>
         </div>
         <Switch>
             <Route path="/list/shoppinglist/bearbeiten">
+                <div className="content-tabs">
                 <div className="Add">
                     <input className="ListInput"></input>
                     <button className="ListButton">+</button>
@@ -32,6 +36,7 @@ export default function ShoppingList() {
                 <ProductChange name="Kekse" amount="3"/>
                 <ProductChange name="Kuchen" amount="5"/>
                 <ProductChange name="Eier" amount="10"/>
+                </div>
             </Route>
             <Route path="/list/shoppinglist/kaufen">
                 <h1>Kaufen</h1>
@@ -41,6 +46,7 @@ export default function ShoppingList() {
                 <ProductBuyChecked name="Butter" amount="1"/>
                 <ProductBuyChecked name="Kekse" amount="3"/>
             </Route>
+            
         </Switch>
     </div>;
 }
