4baff3b3f27916e8d10d64a2cff3483de1f14256
Philipp Kollmar <ph-kollmar@web.de>
2021-05-19 18:18:52 +0200

    #29 deleted unnecessary Regex-Part "\@"

--- src/Login/Register/validators.ts
+++ src/Login/Register/validators.ts
@@ -1,6 +1,6 @@
 export const handleEmail = (txt: string) => {
 
-  const regEx = /^([A-Z|a-z|0-9](\.|-){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/g;
+  const regEx = /^([A-Z|a-z|0-9](\.|-){0,1})+[A-Z|a-z|0-9]@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/g;
 
     if (txt.match(regEx)) {
       return true;
