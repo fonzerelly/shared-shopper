913732722a51cb5e19e008ed2c0631ec35ad087d
Philipp Kollmar <ph-kollmar@web.de>
2021-05-29 18:33:01 +0200

    #47 changed secretCheck to be ready for tests

--- src/Login/backend.ts
+++ src/Login/backend.ts
@@ -1,7 +1,8 @@
 import { secretCheck } from "../secret/secret"
 
 export function LoginToken() {
-    const secret = secretCheck();
+    const search = window.location.search
+    const secret = secretCheck(search);
         return fetch("http://localhost:3000/login", {
             method: "POST",
             headers: {
--- src/secret/secret.ts
+++ src/secret/secret.ts
@@ -1,5 +1,4 @@
-export function secretCheck(){
-    const queryString = window.location.search 
+export function secretCheck(queryString: string){
     const urlParams = new URLSearchParams(queryString)
     const secret = urlParams.get('secret')
     return secret
