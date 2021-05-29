723ac6e69b104fdf32aa89dc14c9fb733dceef70
Philipp Kollmar <ph-kollmar@web.de>
2021-05-29 17:57:44 +0200

    #47 removed consolelog

--- src/Login/backend.ts
+++ src/Login/backend.ts
@@ -2,7 +2,6 @@ import { secretCheck } from "../secret/secret"
 
 export function LoginToken() {
     const secret = secretCheck();
-    console.log(secret)
         return fetch("http://localhost:3000/login", {
             method: "POST",
             headers: {
