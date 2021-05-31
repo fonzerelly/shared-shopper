e0c28abcd715290154730efc33977c0bf822719c
Philipp Kollmar <ph-kollmar@web.de>
2021-05-31 20:32:25 +0200

    #47 splitted regEx

--- src/Login/Register/validators.ts
+++ src/Login/Register/validators.ts
@@ -1,11 +1,19 @@
 export const handleEmail = (txt: string) => {
 
-  const regEx = /^([A-Z|a-z|0-9](\.|-){0,1})+[A-Z|a-z|0-9]@([A-Z|a-z|0-9](-){0,1})+((\.){0,1}[A-Z|a-z|0-9])+\.[a-z]{2,3}$/g;
+  //const regEx = /^([A-Z|a-z|0-9](\.|-){0,1})+[A-Z|a-z|0-9]@([A-Z|a-z|0-9](-){0,1})+((\.){0,1}[A-Z|a-z|0-9])+\.[a-z]{2,3}$/g;
 
-    if (txt.match(regEx)) {
-      return true;
-    }
-    else {
-      return false;
-    }
+  const userNamePart = "^([A-Z|a-z|0-9](\\.|-){0,1})+"
+  const userNameEnd = "[A-Z|a-z|0-9]"
+  const at = "@"
+  const domain = "([A-Z|a-z|0-9](-){0,1})+((\\.){0,1}[A-Z|a-z|0-9])+"
+  const topLevelDomain = "\\.[a-z]{2,3}$"
+
+  const regEx = new RegExp(userNamePart + userNameEnd + at + domain + topLevelDomain, "g")
+
+  if (txt.match(regEx)) {
+    return true;
   }
+  else {
+    return false;
+  }
+}
