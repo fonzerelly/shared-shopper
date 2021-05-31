091187ec61017c096dc903432ef5286f128daea6
Philipp Kollmar <ph-kollmar@web.de>
2021-05-31 21:57:55 +0200

    #47 added first tests

--- src/Login/backend.spec.ts
+++ src/Login/backend.spec.ts
@@ -2,16 +2,24 @@ import { aquireToken } from "./backend"
 import nock from 'nock'
 
 describe("aquireToken", () => {
-    const scope = nock('http://myapi.com')
-        .get('/token')
-        .reply(200, { token: "1111111" },
-            {
-                'Access-Control-Allow-Origin': '*',
-                'Content-type': 'application/json'
-            })
-    const mockAquire = () => "111111"
+    //  nock('http://localhost:3000')
+    //     .post('/login', { email: 'test@email.com', password: "Passwort" })
+    //     .reply(200, { token: "1111111" })
 
-    it("should return accessToken", () => {
-        expect(mockAquire()).toEqual("111111")
+   
+
+
+    it("should return accessToken", async () => {
+        const scope = nock('http://localhost:3001') 
+        .get('/products')
+        .reply(200, { products: [{ id: 1, name: 'nocked data' }] }, 
+        { 
+          'Access-Control-Allow-Origin': '*', 
+          'Content-type': 'application/json' 
+        });
+
+      const result =  await aquireToken("test@email.de", "test")
+      console.log(result + "************************")
+    
     })
 })
