9be6495d63c805a3ff0040b532d26b15d757c20f
fr0zen3ye <stefan.golker@gmx.de>
2021-06-04 13:56:59 +0200

    #47 save accesstoken in session

--- src/Loading/Loading.tsx
+++ src/Loading/Loading.tsx
@@ -1,7 +1,9 @@
 import './Loading.css'
 import { ReactComponent as Logo } from '../img/LogoWhite.svg'
+import { session } from '../Login/session'
 
 export function Loading() {
+    console.log(session.token)
     return <div className="LoaderBody">
         <Logo />
         <div className="Loader"></div>
--- src/Login/Signin/signin.tsx
+++ src/Login/Signin/signin.tsx
@@ -2,28 +2,27 @@ import './signin.css'
 import { useState } from 'react'
 import Logosvg from '../../Components/Logo/Logo'
 import { PrimaryButton } from '../../Components/Buttons/Button'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import InputComp from '../../Components/Input/Input';
 import {aquireToken} from '../backend'
 import '../../Components/Buttons/button.css'
+import { session } from '../session';
 
 export function SignIn() {
-
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
-    const callToken = () => {
-        aquireToken(email, password)
-            .then(console.log)
-       }
-    
+    const history = useHistory()
+    const callToken = async () => {
+        session.token = await aquireToken(email, password)
+        history.push('/')
+        }
     return (
         <div className="signinbody">
             <Logosvg />
             <div className="form">
                 <InputComp label="E-Mail" place="E-Mail" type="email" setter={(txt: string) => {setEmail(txt)}} />
                 <InputComp label="Passwort" place="Passwort" type="password" setter={(txt:string) => {setPassword(txt)}} />
-                <Link to="/list"><button className="SecondaryButton" onClick={() => {callToken()}}>Login</button></Link>
+                <button className="SecondaryButton" onClick={() => {callToken()}}>Login</button>
                 <ORSection />
                 <Link to="register"><PrimaryButton name="Register" /></Link>
             </div>
