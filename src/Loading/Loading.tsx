c63fdb98a5228dd64400efc3f44994be245ff5f6
fr0zen3ye <stefan.golker@gmx.de>
2021-06-07 13:44:28 +0200

    #47 added function for checking if token is available

--- src/Loading/Loading.tsx
+++ src/Loading/Loading.tsx
@@ -1,9 +1,11 @@
 import './Loading.css'
 import { ReactComponent as Logo } from '../img/LogoWhite.svg'
 import { session } from '../Login/session'
+import { CheckTokenAvailable } from './checkTokenAvailable'
 
 export function Loading() {
     console.log(session.token)
+    CheckTokenAvailable()
     return <div className="LoaderBody">
         <Logo />
         <div className="Loader"></div>
