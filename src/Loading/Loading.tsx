198ce53a224bd4664c1e01808f6ed21f21ce59dd
Philipp Kollmar <ph-kollmar@web.de>
2021-06-21 13:46:28 +0200

    #47 added aquireToken test

--- src/Loading/Loading.tsx
+++ src/Loading/Loading.tsx
@@ -4,7 +4,6 @@ import { session } from '../Login/session'
 import { CheckTokenAvailable } from './checkTokenAvailable'
 
 export function Loading() {
-    console.log(session.token)
     CheckTokenAvailable()
     return <div className="LoaderBody">
         <Logo />
--- src/Login/backend.spec.ts
+++ src/Login/backend.spec.ts
@@ -1,18 +1,22 @@
 import { aquireToken } from "./backend"
-// import nock from 'nock'
+import nock from 'nock'
 
- describe("aquireToken", () => {
-//     const scope = nock('http://myapi.com')
-//         .get('/token')
-//         .reply(200, { token: "1111111" },
-//             {
-//                 'Access-Control-Allow-Origin': '*',
-//                 'Content-type': 'application/json'
-//             })
-//     const mockAquire = () => "111111"
+describe("aquireToken", () => {
+    it("should return accessToken", async () => {
+        const scope = nock('http://localhost:3000')
+        .options("/login")
+        .reply(200, () => null, {
+          "Access-Control-Allow-Origin": "*",
+          'access-control-allow-headers': 'x-shared-shopper-secret',
+          "Content-Type": "application:json"
+        })
+        .post('/login', {email: `test@email.de`, password: 'test'})
+        .reply(200, { accessToken: '1111111' }, 
+        { 
+          'Access-Control-Allow-Origin': '*',
+        });
 
-//     it("should return accessToken", () => {
-//         expect(mockAquire()).toEqual("111111")
-//     })
-        it("", () => {})
- })
+      const result =  await aquireToken("test@email.de", "test")
+      expect(result).toEqual('1111111')
+    })
+})
