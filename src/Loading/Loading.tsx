53af3a7c513f7edb78439d34d46f6b74e9bb97ab
Philipp Kollmar <ph-kollmar@web.de>
2021-06-21 17:16:48 +0200

    removed compiler error

--- src/Loading/Loading.tsx
+++ src/Loading/Loading.tsx
@@ -1,6 +1,5 @@
 import './Loading.css'
 import { ReactComponent as Logo } from '../img/LogoWhite.svg'
-import { session } from '../Login/session'
 import { CheckTokenAvailable } from './checkTokenAvailable'
 
 export function Loading() {
