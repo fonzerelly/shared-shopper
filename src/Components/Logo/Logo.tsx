071a5e6a046b97a6e2c6924912bff31614f05c67
fr0zen3ye <stefan.golker@gmx.de>
2021-07-02 17:19:16 +0200

    #2 tests for signin

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import {ReactComponent as Logosvg} from '../../img/Logo.svg'
 import './logo.css'
 
 export default function Logo () {
-    return <div className="logoPlace"> 
+    return <div className="logoPlace" data-testid="logoTest"> 
     <Logosvg/>
     <div className="logoText">Shared Shopper</div>
     </div>
---
+++ src/Login/Signin/signin.spec.tsx
@@ -0,0 +1,65 @@
+import { render, screen, fireEvent, cleanup } from "@testing-library/react"
+import { SignIn } from "./signin"
+import { callToken } from "./signin";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+import { useState } from "react";
+
+function TestEnvironment(props: { children: any }) {
+    return <Router>
+        <Switch>
+            <Route>
+                {props.children}
+            </Route>
+        </Switch>
+    </Router>
+}
+
+describe('SignIn', () => {
+    beforeEach(() => {
+        return render(
+            <TestEnvironment>
+                <SignIn />
+            </TestEnvironment>
+        )
+    })
+    it('should render logo', () => {
+        const logo = screen.getByTestId("logoTest")
+        expect(logo).toHaveClass("logoPlace")
+    })
+    it("should render input field", () => {
+        screen.getAllByTestId('inputComp')
+    })
+    it("should render E-Mail field", () => {
+        screen.getByPlaceholderText('E-Mail')
+    })
+    it("should render Passwort field", () => {
+        screen.getByPlaceholderText('Passwort')
+    })
+    it('should render Login button', () => {
+        const loginButton = screen.getByTestId("SecondaryButton")
+        expect(loginButton).toHaveClass("SecondaryButton")
+    })
+    it('should render register button', () => {
+        const registerButton = screen.getByTestId("PrimaryButton")
+        expect(registerButton).toHaveClass("PrimaryButton")
+    })
+    it('should render register button', () => {
+        const registerButtonLink = screen.getByTestId("registerButton")
+        expect(registerButtonLink).toHaveAttribute("href", "/register")
+    })
+    it("should render ORSection text ODER", () => {
+        screen.getByText("ODER")
+    })
+    it('should render line1', () => {
+        const line1Test = screen.getByTestId("line1")
+        expect(line1Test).toHaveClass("line")
+    })
+    it('should render line2', () => {
+        const line2Test = screen.getByTestId("line2")
+        expect(line2Test).toHaveClass("line")
+    })
+})
